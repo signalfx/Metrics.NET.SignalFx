352d77ed260bc399bcd07e3bee54846b2740a7ec
Ted Crossman <tedo@signalfuse.com>
2014-12-01 16:11:37 -0800

    Add support for configuring from App.Config

--- SignalFxExtensions.cs
+++ SignalFxExtensions.cs
@@ -1,8 +1,11 @@
-﻿using Metrics.Reports;
+﻿using Metrics.Logging;
+using Metrics.Reports;
 using Metrics.SignalFx;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace Metrics
@@ -11,6 +14,7 @@ namespace Metrics
     {
         private static readonly string DEFAULT_URI = "https://api.signalfuse.com";
         private static readonly string INSTANCE_ID_DIMENSION = "InstanceId";
+        private static readonly ILog log = LogProvider.GetCurrentClassLogger();
 
         public class SignalFxReporterBuilder
         {
@@ -88,6 +92,52 @@ namespace Metrics
         public static SignalFxReporterBuilder WithSignalFx(this MetricsReports reports, string apiToken, string baseURI, TimeSpan interval)
         {
             return new SignalFxReporterBuilder(reports, apiToken, interval, baseURI);
+        }
+
+        public static MetricsReports WithSignalFxFromAppConfig(this MetricsReports reports)
+        {
+            try
+            {
+                var apiToken = ConfigurationManager.AppSettings["Metrics.SignalFx.APIToken"];
+                var signalFxMetricsInterval = ConfigurationManager.AppSettings["Metrics.SignalFx.Interval.Seconds"];
+                var signalFxSourceType = ConfigurationManager.AppSettings["Metrics.SignalFx.Source.Type"];
+                var signalFxSourceValue = ConfigurationManager.AppSettings["Metrics.SignalFx.Source.Value"];
+                if (!string.IsNullOrEmpty(apiToken) && !string.IsNullOrEmpty(signalFxMetricsInterval) && !string.IsNullOrEmpty(signalFxSourceType))
+                {
+                    int seconds;
+                    if (int.TryParse(signalFxMetricsInterval, out seconds) && seconds > 0)
+                    {
+                        var defaultDimensions = (ConfigurationManager.GetSection("SignalFx/defaultDimensions") as System.Collections.Hashtable)
+                             .Cast<System.Collections.DictionaryEntry>()
+                             .ToDictionary(n => n.Key.ToString(), n => n.Value.ToString());
+                        SignalFxReporterBuilder builder = new SignalFxReporterBuilder(reports, apiToken, defaultDimensions, TimeSpan.FromSeconds(seconds));
+                        switch (signalFxSourceType)
+                        {
+                            case "netbios":
+                                return builder.WithNetBiosNameSource();
+                            case "dns":
+                                return builder.WithDNSSource();
+                            case "fqdn":
+                                return builder.WithFQDNSource();
+                            case "source":
+                                if (!string.IsNullOrEmpty(signalFxSourceValue))
+                                {
+                                    return builder.WithSource(signalFxSourceValue);
+                                }
+                                throw new Exception("Metrics.SignalFx.Source.Value must be set if Metrics.SignalFx.Source.Type is \"source\".");
+                            default:
+                                throw new Exception("Metrics.SignalFx.Source.Type must be one of netbios, dns, fqdn, or source(with Metrics.SignalFx.Source.Value set)");
+                        }
+                    }
+                }
+                return reports;
+            }
+            catch (Exception x)
+            {
+                log.ErrorException("Metrics: Error configuring SignalFx reports", x);
+                throw new InvalidOperationException("Invalid Metrics Configuration: Metrics.SignalFx.APIToken must be non-empty and Metrics.SignalFx.Interval.Seconds must be an integer > 0", x);
+
+            }
         }
 
     }
