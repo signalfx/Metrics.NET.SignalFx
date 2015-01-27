88125d4497d0e9e087ad1bc8aa305e4d87a5b8e9
Ted Crossman <tedo@signalfuse.com>
2015-01-27 15:07:32 -0800

    Add ability to have metrics with the same name with different tags
    The default MetricsContexts/Registry provided by Metrics.NET
    Only uses the name to dedup metrics. For SignalFx use
    the tags should also be considered when uniquifying metrics.
    
    using TaggedMetricContext will allow this.

---
+++ Metrics.NET.SignalFX/TaggedMetricContext.cs
@@ -0,0 +1,25 @@
+﻿using Metrics.Core;
+using Metrics.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metrics.SignalFX
+{
+    public class TaggedMetricsContext : BaseMetricsContext
+    {
+        public TaggedMetricsContext()
+            : this(string.Empty) { }
+
+        public TaggedMetricsContext(string context)
+            : base(context, new TaggedMetricsRegistry(), new DefaultMetricsBuilder(), () => Clock.Default.UTCDateTime)
+        { }
+
+        protected override MetricsContext CreateChildContextInstance(string contextName)
+        {
+            return new TaggedMetricsContext(contextName);
+        }
+    }
+}
