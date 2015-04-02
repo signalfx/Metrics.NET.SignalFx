3784d3fb8c76cb37e1d2d70bf11cd7c832af655b
Ted Crossman <tedo@signalfuse.com>
2015-04-02 16:30:43 -0700

    Upgrade to version 0.0.3.0

--- Metrics.NET.SignalFX/Properties/AssemblyInfo.cs
+++ Metrics.NET.SignalFX/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("0.0.3.0")]
+[assembly: AssemblyFileVersion("0.0.3.0")]
 [assembly: InternalsVisibleTo("Metrics.NET.SignalFx.UnitTest")]
